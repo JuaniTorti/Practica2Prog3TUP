38a82e7176f61c35cc710cd967e02adba1e945b0
JuaniTorti <nachi.tortjuani@gmail.com>
2024-04-24 13:12:38 -0300

    Create AgilityCharacter.cs

---
+++ Web/Ej4/AgilityCharacter.cs
@@ -0,0 +1,20 @@
+﻿namespace Web.Ej4
+{
+    public class AgilityCharacter : Character
+    {
+        public AgilityCharacter(string name,float force, float agility, float magic)
+        {
+            Name = name;
+            Agility = agility;
+            Magic = magic;
+            Force = force;
+        }
+
+        public override float CalculateDamage()
+        {
+            float damage = (Force/2) * Agility * (Magic/2);
+            return damage;
+
+        }
+    }
+}
