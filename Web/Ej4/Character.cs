d1b3267dd2d6d94537635cb06cfdf83b002c3338
JuaniTorti <nachi.tortjuani@gmail.com>
2024-04-24 13:12:32 -0300

    Create Character.cs

---
+++ Web/Ej4/Character.cs
@@ -0,0 +1,17 @@
+﻿namespace Web.Ej4
+{
+    public abstract class Character 
+    {
+        public string Name { get; set; }
+        public float Force { get; set; }
+        public float Agility { get; set; }
+        public float Magic { get; set; }
+
+        public abstract float CalculateDamage();
+        public string Atack()
+        {
+            return $"{Name} hizo {CalculateDamage()} de daño";
+        }
+        
+    }
+}
