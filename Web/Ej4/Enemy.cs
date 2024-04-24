11b171f28f4ac69c2eb0eaadd9a908764ef5a190
JuaniTorti <nachi.tortjuani@gmail.com>
2024-04-24 13:12:45 -0300

    Create Enemy.cs

---
+++ Web/Ej4/Enemy.cs
@@ -0,0 +1,14 @@
+﻿namespace Web.Ej4
+{
+    public class Enemy
+    {
+        public float HealthPoints { get; set; }
+        public int Level { get; set; }
+
+        public Enemy(int level = 1, float healPoints = 1000)
+        {          
+            Level = level;
+            HealthPoints = healPoints * level;
+        }
+    }
+}
