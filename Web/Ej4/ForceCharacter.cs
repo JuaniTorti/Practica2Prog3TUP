9edc9aff582006e074a6b4edc34af874c779a8e1
JuaniTorti <nachi.tortjuani@gmail.com>
2024-04-24 13:12:35 -0300

    Create ForceCharacter.cs

---
+++ Web/Ej4/ForceCharacter.cs
@@ -0,0 +1,22 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Web.Ej4
+{
+    public class ForceCharacter : Character
+    {
+        public ForceCharacter(string name,float agility, float magic) 
+        {
+            Name = name;
+            Agility = agility;
+            Magic = magic;
+            Force = 100;
+        }
+
+        public override float CalculateDamage()
+        {
+            float damage = (Force*2 - Agility) * Magic;
+            return damage;
+
+        }
+    }
+}
