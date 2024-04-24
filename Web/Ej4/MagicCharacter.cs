fff3309923420aa29625d22b10d05f252a164cc8
JuaniTorti <nachi.tortjuani@gmail.com>
2024-04-24 13:12:43 -0300

    Create MagicCharacter.cs

---
+++ Web/Ej4/MagicCharacter.cs
@@ -0,0 +1,25 @@
+﻿namespace Web.Ej4
+{
+    public class MagicCharacter : Character
+    {
+        public MagicCharacter(string name, float force, float agility, float magic = 60)
+        {
+            Name = name;
+            Agility = agility;
+            Magic = magic;
+            Force = force;
+        }
+
+        public override float CalculateDamage()
+        {
+            float damage = Force + Agility + Magic*4;
+            return damage;
+
+        }
+
+        public new string Atack()
+        {
+            return $"{Name} hizo {CalculateDamage()} de daño gracias a su magia";
+        }
+    }
+}
